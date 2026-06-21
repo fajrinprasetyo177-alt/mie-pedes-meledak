7f183015eab2880989543a19197a04c6420a7bd7
fajrinprasetyo177-alt <fajrinprasetyo177@gmail.com>
2026-06-21 21:55:50 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function login(){
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username == "admin" && password == "12345"){
+        alert("Login berhasil");
+
+        window.location.href = "dashboard.html";
+    }
+    else{
+        document.getElementById("pesan").innerHTML =
+        "Username atau password salah!";
+    }
+
+}
